e1d5cd4ef70c85e60c23b176b60db3a3afb4e8c1
Fardin Abu Ubaid <103235862+FardinUbaid@users.noreply.github.com>
2024-02-05 17:52:54 +0600

    Add files via upload

---
+++ 3_dataTypesView.py
@@ -0,0 +1,14 @@
+var1 = 100
+var2 = 20.55
+var3 = "Fardin"
+var4 = 'Abu Ubaid'
+var5 = 20+5j
+var6 = True
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+print(type(var5))
+print(type(var6))
+
---
+++ 4_concatenation.py
@@ -0,0 +1,14 @@
+# Simple
+print("Hi! " + "This is Fardin")
+
+# using variables
+greetings = "Hi! "
+name = "Fardin"
+
+print(greetings + name)
+# note that + must be used for same type of datatype only
+# with extra texts
+print(greetings + "This is " + name)
+
+# we can use (,) too. Comma adds a blank space for each use .
+print(greetings,"this is",name)
