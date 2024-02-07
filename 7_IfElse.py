ce3f458b6e192f40f6522d7d52eebd1a9daace40
Fardin Abu Ubaid <103235862+FardinUbaid@users.noreply.github.com>
2024-02-07 21:05:13 +0600

    Add files via upload

---
+++ 7_IfElse.py
@@ -0,0 +1,19 @@
+# basic if else
+
+num1 = 10
+num2 = 20
+
+if(num1<num2):
+    print(str(num1)+" is less than "+str(num2))
+else:
+    print(str(num2) + " is less than " + str(num1))
+
+
+
+#text="fardin"
+text = "Fardin"
+
+if(text == "fardin"):
+    print("My name is "+text)
+else:
+    print("Name did't match!")
---
+++ 8_ElseIf.py
@@ -0,0 +1,10 @@
+#number = 77
+#number = 49
+number = 100
+
+if(number<50):
+    print(str(number)+" is less than 50")
+elif(number==70):
+    print(str(number) + " is greater than 69")
+elif(number==100):
+    print(str(number) + " is equal to 100")
